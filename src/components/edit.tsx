5e3451d6a96c69544e35c9e44c1b94eb95bf079f
violaine-alltricks <violaine.ballandin@alltricks.com>
2021-10-19 11:53:12 +0200

    implement sub page with query string. use SSR

--- src/components/edit.tsx
+++ src/components/edit.tsx
@@ -2,23 +2,25 @@ import { Button } from 'antd';
 import styles from './add-edit.module.css';
 import Form from './form';
 
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type EditProps = {
   status: 'open' | 'close';
   item: Record<string, string>;
 };
 
-const Edit: React.FC<EditProps> = ({ status, item }) => (
-  <div className={styles[status]}>
-    <Link href="/list">
-      <a>
-        <Button type="danger">X</Button>
-      </a>
-    </Link>
+const Edit: React.FC<EditProps> = ({ status, item }) => {
+  const router = useRouter();
 
-    <Form mode="edit" item={item} />
-  </div>
-);
+  return (
+    <div className={styles[status]}>
+      <Button type="danger" onClick={() => router.push('/list', '/list', { shallow: true })}>
+        X
+      </Button>
+
+      <Form mode="edit" item={item} />
+    </div>
+  );
+};
 
 export default Edit;
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -4,14 +4,16 @@ import { useRouter } from 'next/router';
 import Add from '../../components/add';
 import Edit from '../../components/edit';
 
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
 
-const List: React.FC = () => {
+type ListProps = {
+  sw: Record<string, string>[];
+};
+
+const List: React.FC<ListProps> = ({ sw }) => {
   const router = useRouter();
-  const [asFetch, setAsFetch] = useState(false);
   const [panel, setPanel] = useState('close');
-  const [sw, setSw] = useState<Record<string, string>[]>([]);
 
   useEffect(() => {
     const p = router.query.mode ?? ('close' as string);
@@ -19,45 +21,56 @@ const List: React.FC = () => {
     setPanel(p as string);
   }, [router]);
 
-  useEffect(() => {
-    fetch('https://swapi.dev/api/people')
-      .then((res) => res.json())
-      .then((body) => {
-        setSw(body.results);
-        setAsFetch(true);
-      });
-  }, []);
-
   return (
     <>
       <Add status={panel === 'add' ? 'open' : 'close'} />
       <Edit
         status={panel === 'edit' ? 'open' : 'close'}
-        item={sw.find((e) => e.name === router.query.slug?.[1])}
+        item={sw.find((e) => e.name === router.query.id) ?? {}}
       />
 
-      <Link href="/list?mode=add">
-        <a>
-          <Button type="primary">Show add</Button>
-        </a>
-      </Link>
+      <Button
+        type="primary"
+        onClick={() => router.push('/list?mode=add', '/list?mode=add', { shallow: true })}
+      >
+        Show add
+      </Button>
 
       <div>List Page</div>
 
       <ul>
         {sw.map((e) => (
           <li key={e.name}>
-            <Link href={`/list?mode=edit&id=${e.name}`}>
-              <a>
-                <Button type="primary">Edit</Button>
-              </a>
-            </Link>
+            <Button
+              type="primary"
+              onClick={() =>
+                router.push(`/list?mode=edit&id=${e.name}`, `/list?mode=edit&id=${e.name}`, {
+                  shallow: true,
+                })
+              }
+            >
+              Edit
+            </Button>
             {e.name}
           </li>
         ))}
       </ul>
     </>
   );
+};
+
+// Because authentication check is done on client-side, this is trigger even is the user is not authenticated.
+// Is it possible to check for user authentication on the server?
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Maybe this works here because the request is not authenticated. Need to check how to access authentication on server side.
+  const res = await fetch('https://swapi.dev/api/people');
+  const body = await res.json();
+
+  return {
+    props: {
+      sw: body.results,
+    },
+  };
 };
 
 export default List;
